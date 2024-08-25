53b3cacaaa03346052c25c7d7b12c3f6521454d2
Krishnakumar Chandran <mail2krishnachandran@gmail.com>
2024-08-25 02:13:53 -0700

    Added htmx code with template blocks

--- cmd/main.go
+++ cmd/main.go
@@ -32,9 +32,12 @@ func main() {
 
 	count := Count{Count: 0}
 	e.GET("/", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "index.html", count)
+		return c.Render(200, "index", count)
 	})
 	
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "count", count)
+	})
 	e.Logger.Fatal(e.Start(":42069"))
 }
